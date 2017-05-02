317dc81c3d63d2ebb2c7e791addbe650ad5d6888
bang <sqibang@gmail.com>
2017-05-02 22:42:17 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using aspnet_core_mssql_ef_demos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_mssql_ef_demos.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly NorthWindContext _context;
+
+		public HomeController(NorthWindContext context)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var employees = await _context.Employees.Take(5).AsNoTracking().ToListAsync();
+
+			return View(employees);
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+	}
+}
---
+++ db.sql
@@ -0,0 +1,3 @@
+SELECT TOP(5) [e].[EmployeeID], [e].[Address], [e].[BirthDate], [e].[City], [e].[Country], [e].[Extension], [e].[FirstName]
+, [e].[HireDate], [e].[HomePhone], [e].[LastName], [e].[Notes], [e].[Photo], [e].[PhotoPath], [e].[PostalCode], [e].[Region], [e].[ReportsTo], [e].[Title], [e].[TitleOfCourtesy]
+      FROM [Employees] AS [e]
